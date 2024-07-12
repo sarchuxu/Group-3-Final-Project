cf5ab94a72f8afc13dd5769fa6d1f859e129d116
Sarah Xu <sar.chu.xu@gmail.com>
2024-07-12 13:26:06 -0700

    Initial Commit

---
+++ Third Try/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Third Try
+//
+//  Created by Sarah Xu on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Third Try/Third_TryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Third_TryApp.swift
+//  Third Try
+//
+//  Created by Sarah Xu on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Third_TryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
